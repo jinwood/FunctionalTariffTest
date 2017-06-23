9fec855d92e77f852afef16566ee6bf60d5eb0e4
jinwood <jinw@protonmail.com>
2017-06-23 11:41:41 +0100

    Remove some debug code

--- TarrifComparison/UsageLogic.cs
+++ TarrifComparison/UsageLogic.cs
@@ -8,7 +8,6 @@ namespace TarrifComparison
     {
         public static void CalculateUsage(string[] args, List<TarrifEntry> tarrifs, Action<string> output)
         {
-
             var tarrifName = args[1];
             var fuelType = args[2];
             var targetMonthlySpend = decimal.Parse(args[3]);
@@ -16,10 +15,6 @@ namespace TarrifComparison
             Func<TarrifEntry, decimal> calculateSpend =
                 (tarrif) => (targetMonthlySpend / (fuelType == "gas" ? tarrif.rates.gas : tarrif.rates.power)
                             + tarrif.standing_charge).ApplyVAT() * 12;
-
-            var xb = tarrifs.Where(xa =>
-                xa.tariff == tarrifName)
-                    .Select(y => $"ANNUAL_{fuelType.ToUpper()}_USAGE {calculateSpend(y).ToString("#.##")} KW/h").FirstOrDefault().ToString();
 
             output(tarrifs.Where(x =>
                 x.tariff == tarrifName)
